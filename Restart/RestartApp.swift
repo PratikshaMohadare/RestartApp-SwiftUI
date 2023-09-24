619d020815c9a92a0179f2b77a24a20056f7b469
Pratiksha <pratiksha.mohadare9@gmail.com>
2023-09-24 19:52:48 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Pratiksha on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
